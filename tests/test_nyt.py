be19816bad138048a279673cd69adc1957619a1e
Ryan Nelson <ryan@nelsonr.dev>
2025-07-24 18:02:45 -0700

    Add test_card()

--- tests/test_nyt.py
+++ tests/test_nyt.py
@@ -1,4 +1,4 @@
-from connector.get import *
+from connector.connector import *
 
 def test_get_puzzle():
     puzzle = GoldenRetriever('2025-07-22')
@@ -6,8 +6,11 @@ def test_get_puzzle():
     assert puzzle.raw == """{"status":"OK","id":811,"print_date":"2025-07-22","editor":"Wyna Liu","categories":[{"title":"SASSINESS","cards":[{"content":"ATTITUDE","position":7},{"content":"CHEEK","position":1},{"content":"LIP","position":8},{"content":"MOUTH","position":14}]},{"title":"MITIGATE","cards":[{"content":"CUSHION","position":3},{"content":"DAMPEN","position":12},{"content":"SOFTEN","position":9},{"content":"TEMPER","position":4}]},{"title":"PATTERNS","cards":[{"content":"CAMO","position":13},{"content":"CHECKERS","position":5},{"content":"HONEYCOMB","position":10},{"content":"STRIPES","position":15}]},{"title":"___ PIE","cards":[{"content":"CHESS","position":0},{"content":"CUTIE","position":11},{"content":"HUMBLE","position":6},{"content":"WHOOPIE","position":2}]}]}"""
 
 
-def test_nytpuzzle():
-    pass
+def test_card():
+    puzzle = GoldenRetriever('2025-07-22')
+    solver = puzzle.unsolve()
+    assert(any([(card.content == "MOUTH" and card.position == 14) for card in solver.cards]))
 
-def test_nytcard():
+
+def test_nytpuzzle():
     pass
